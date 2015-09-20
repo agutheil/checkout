d881a2926b5b1d614679d4dd23eef4f688640086
Andreas Gutheil <andreas.gutheil@namics.com>
2015-09-20 22:49:17 +0200

    removes rest interface from articles

--- src/main/java/com/mightymerce/checkout/ArticleRepository.java
+++ src/main/java/com/mightymerce/checkout/ArticleRepository.java
@@ -2,9 +2,7 @@ package com.mightymerce.checkout;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.repository.query.Param;
-import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
-@RepositoryRestResource(collectionResourceRel = "article", path = "article")
 public interface ArticleRepository extends MongoRepository<Article, String>{
 
 	Article findByArticleId(@Param("articleId") Long articleId);
